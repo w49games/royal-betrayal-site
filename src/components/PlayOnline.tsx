be8147609a11b6f998ee7a5e9d5ab31aa3383f5d
W49 Games <w49games@gmail.com>
2026-08-11 13:57:45 +1000

    Update PlayOnline.tsx

--- src/components/PlayOnline.tsx
+++ src/components/PlayOnline.tsx
@@ -83,7 +83,7 @@ export function PlayOnline() {
             </div>
 
             <motion.a
-              href="https://tabletopia.com/games/royalbetrayal-attackofwolloofy-4xuty9/680x340"
+              href="https://tabletopia.com/games/royalbetrayal-attackofwolloofy"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05, boxShadow: '0 0 40px rgba(242, 117, 15, 0.5)' }}
