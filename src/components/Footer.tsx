3c81a2a7893698969ab8a1adcd9a146a5e847727
W49 Games <w49games@gmail.com>
2026-08-11 10:23:42 +1000

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ export function Footer() {
             className="flex items-center gap-6 text-sm font-sans text-secondary-400"
           >
             <a
-              href="https://tabletopia.com/games/royalbetrayal-6d8zi1/play-now"
+              href="https://tabletopia.com/games/royalbetrayal-attackofwolloofy-4xuty9/680x340"
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-primary-400 transition-colors flex items-center gap-1"
